617769993c34886fc2d03e44669253f754626b24
hritikpatidar <hritikpatidar1810@gmail.com>
2022-03-06 14:58:53 +0530

    header

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -14,11 +14,10 @@ export default function Header() {
   }
   return (
     <div>
-     
-      
+  
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container-fluid me-5 ms-5">
-          <a className="navbar-brand me-5" href="#">Ritik Patidar</a>
+        <div className="container-fluid ms-5 me-5">
+          <a className="navbar-brand" href="#">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
@@ -26,21 +25,26 @@ export default function Header() {
             <Navbar className="navbar-nav me-auto mb-2 mb-lg-0">
               <Nav className="nav-item">
                 <Link className="nav-link " to="/"  >Home</Link>
+              </Nav>
+              <Nav className="nav-item">
                 <Link to="/get_teacher" className='nav-link'>Get Teacher</Link>
-                <Link to="/register" className='nav-link'>About Us</Link>
-                <Link to="/register" className='nav-link'>Contact</Link>
               </Nav>
             
+              <Nav className="nav-item">
+                <Link to="/register" className='nav-link'>About Us</Link>
+              </Nav>
+              <Nav className="nav-item">
+                <Link to="/register" className='nav-link'>Contact</Link>
+              </Nav>
             </Navbar>
             <form className="d-flex">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="button">Search</button>
-              <Link to="" className='btn btn-outline-success ms-2' onClick={ (e)=>{ logout(e)} }>Logout</Link>
+              <button className="btn btn-outline-success" type="submit">Search</button>
+              <Link to="#" className='btn btn-outline-success ms-2' onClick={ (e)=>{ logout(e)} }>Logout</Link>
             </form>
           </div>
         </div>
       </nav>
-
 
       
 
